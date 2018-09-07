f50693bc1f42ae5c41465ae2b3a22d558a6488d3
izudinalqasam <ibrohim.gariskeras@gmail.com>
2018-09-07 22:44:21 +0700

    Final Test 1

---
+++ src/UjianLogic.java
@@ -0,0 +1,10 @@
+public class UjianLogic {
+    public static void main(String[] args) {
+
+        for (int i = 4; i < 200; i = i + 4) {
+            if (i != 4) {
+                System.out.print(i);
+            }
+        }
+    }
+}
