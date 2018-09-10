79fd25f4a0b7a11c7854a87b80636784effb55ab
izudinalqasam <ibrohim.gariskeras@gmail.com>
2018-09-10 20:47:28 +0700

    Final Test 1

--- src/UjianLogic.java
+++ src/UjianLogic.java
@@ -1,10 +1,14 @@
 public class UjianLogic {
     public static void main(String[] args) {
 
+        int total=0;
+
         for (int i = 4; i < 200; i = i + 4) {
             if (i != 4) {
-                System.out.print(i);
+                System.out.print(i+" ");
+                total = total+i;
             }
         }
+        System.out.print("\nTotal Seluruh Angka Antara 4 - 200 Keliapatan 4 adalah = " + total);
     }
 }
